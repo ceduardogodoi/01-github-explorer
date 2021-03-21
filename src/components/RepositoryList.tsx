2a5d345d4a358636f1fe49cf200c45d980cc3fa3
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-03-20 23:04:37 -0300

    ignite: finaliza chapter 1

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,16 +4,20 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
       .then((response) => response.json())
       .then((data) => setRepositories(data));
   }, []);
-
-  // Chamadas HTTP - Aula 03, 03:25
 
   return (
     <section className="repository-list">
@@ -25,7 +29,7 @@ export function RepositoryList() {
         ) : (
           <>
             {repositories.map((repository) => (
-              <RepositoryItem key={repository.id} repository={repository} />
+              <RepositoryItem key={repository.name} repository={repository} />
             ))}
           </>
         )}
